5c2263e166256529155bb6660f92fc02f72113f7
Italo A <italoaurelior@gmail.com>
2022-07-14 07:10:54 -0300

    style: list hand cards in grid

--- src/components/HandList/styles.ts
+++ src/components/HandList/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+export const ListContent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+`
